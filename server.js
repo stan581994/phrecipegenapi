b07c2eb8867a973818b472799f1b823cb912e30a
steven-tan-telus <steven.tan@telus.com>
2024-12-13 01:55:26 -0500

    added cors policy

--- server.js
+++ server.js
@@ -1,7 +1,10 @@
 const express = require("express");
 const app = express();
+const cors = require("cors");
 const port = 5000;
 const recipes = require("./recipes");
+
+app.use(cors());
 
 app.get("/recipe/all", (req, res) => {
   res.json(recipes);
