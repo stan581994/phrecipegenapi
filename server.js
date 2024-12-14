9a890ddf50e905462529cb9ceda3b20ae05a387e
steven-tan-telus <steven.tan@telus.com>
2024-12-14 10:26:38 -0500

    added dessert and serch per category

--- server.js
+++ server.js
@@ -10,6 +10,19 @@ app.get("/recipe/all", (req, res) => {
   res.json(recipes);
 });
 
+app.get("/recipe/category/:category", (req, res) => {
+  const category = req.params.category.toLowerCase();
+  const filteredRecipes = recipes.filter(
+    (recipe) => recipe.category.toLowerCase() === category
+  );
+
+  if (filteredRecipes.length === 0) {
+    res.status(404).send("No recipes found for this category.");
+  } else {
+    res.json(filteredRecipes);
+  }
+});
+
 app.get("/recipe/:dishName", (req, res) => {
   const dishName = req.params.dishName;
   const recipe = recipes.find((recipe) => recipe.dishName === dishName);
